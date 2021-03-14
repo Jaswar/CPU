34c88135cec0c9a9d8386a532166d36d126fd2f6
Jan Warchocki <j.warchocki@student.tudelft.nl>
2021-03-14 23:22:14 +0100

    Added the logic unit with the tests

---
+++ src/main/circuits/LogicUnit.java
@@ -0,0 +1,113 @@
+package main.circuits;
+
+import jdk.jshell.Snippet;
+import main.BitStream;
+import main.Node;
+import main.gates.TriState;
+import main.gates.binary.*;
+import main.gates.unary.NOT;
+
+import java.util.List;
+
+public class LogicUnit implements Circuit {
+
+    private BitStream source;
+    private BitStream destination;
+    private BitStream output;
+    private List<BitStream> controls;
+    private String name;
+    private boolean inDebuggerMode;
+    private int debugDepth;
+
+    public LogicUnit(BitStream source, BitStream destination,
+                     BitStream output, List<BitStream> controls,
+                     String name, boolean inDebuggerMode, int debugDepth) {
+        this.source = source;
+        this.destination = destination;
+        this.output = output;
+        this.controls = controls;
+        this.name = name;
+        this.inDebuggerMode = inDebuggerMode;
+        this.debugDepth = debugDepth;
+
+        this.build();
+    }
+
+    public LogicUnit(BitStream source, BitStream destination,
+                     BitStream output, List<BitStream> controls,
+                     String name) {
+        this(source, destination, output, controls, name, false, 0);
+    }
+
+    public LogicUnit(BitStream source, BitStream destination,
+                     BitStream output, List<BitStream> controls,
+                     boolean inDebuggerMode, int debugDepth) {
+        this(source, destination, output, controls, "Logic Unit", inDebuggerMode, debugDepth);
+    }
+
+    public LogicUnit(BitStream source, BitStream destination,
+                     BitStream output, List<BitStream> controls) {
+        this(source, destination, output, controls, "Logic Unit", false, 0);
+    }
+
+    public BitStream getSource() {
+        return source;
+    }
+
+    public BitStream getDestination() {
+        return destination;
+    }
+
+    public BitStream getOutput() {
+        return output;
+    }
+
+    public List<BitStream> getcontrols() {
+        return controls;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isInDebuggerMode() {
+        return inDebuggerMode;
+    }
+
+    public int getDebugDepth() {
+        return debugDepth;
+    }
+
+    @Override
+    public void build() {
+        boolean debugGates = this.debugDepth > 0 ? this.inDebuggerMode : false;
+        int size = this.source.getSize();
+
+        BitStream notToTriState = new BitStream(size);
+        BitStream orToTriState = new BitStream(size);
+        BitStream andToTriState = new BitStream(size);
+        BitStream xorToTriState = new BitStream(size);
+        BitStream nandToTriState = new BitStream(size);
+        BitStream norToTriState = new BitStream(size);
+
+        NOT not = new NOT(this.source, notToTriState, "mainNot", debugGates);
+        OR or = new OR(this.source, this.destination, orToTriState, "mainOr", debugGates);
+        AND and = new AND(this.source, this.destination, andToTriState, "mainAnd", debugGates);
+        XOR xor = new XOR(this.source, this.destination, xorToTriState, "mainXor", debugGates);
+        NAND nand = new NAND(this.source, this.destination, nandToTriState, "mainNand", debugGates);
+        NOR nor = new NOR(this.source, this.destination, norToTriState, "mainNor", debugGates);
+
+        TriState notTriState = new TriState(notToTriState, this.controls.get(0), this.output,
+                "notTriState", debugGates);
+        TriState orTriState = new TriState(orToTriState, this.controls.get(1), this.output,
+                "orTriState", debugGates);
+        TriState andTriState = new TriState(andToTriState, this.controls.get(2), this.output,
+                "andTriState", debugGates);
+        TriState xorTriState = new TriState(xorToTriState, this.controls.get(3), this.output,
+                "xorTriState", debugGates);
+        TriState nandTriState = new TriState(nandToTriState, this.controls.get(4), this.output,
+                "nandTriState", debugGates);
+        TriState norTriState = new TriState(norToTriState, this.controls.get(5), this.output,
+                "norTriState", debugGates);
+    }
+}
---
+++ test/main/circuits/LogicUnitTest.java
@@ -0,0 +1,154 @@
+package main.circuits;
+
+import main.BitStream;
+import main.Node;
+import main.control.Input;
+import main.control.Output;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicUnitTest {
+
+    Input sourceInput, destinationInput, notControlInput, orControlInput, andControlInput,
+            xorControlInput, nandControlInput, norControlInput;
+    Output outputObject;
+
+    void run(List<Node> queue) {
+        while (queue.size() > 0) {
+            Node node = queue.remove(0);
+            node.evaluate(queue);
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        BitStream source = new BitStream(4);
+        BitStream destination = new BitStream(4);
+        BitStream output = new BitStream(4);
+
+        BitStream notControl = new BitStream(1);
+        BitStream orControl = new BitStream(1);
+        BitStream andControl = new BitStream(1);
+        BitStream xorControl = new BitStream(1);
+        BitStream nandControl = new BitStream(1);
+        BitStream norControl = new BitStream(1);
+
+        List<BitStream> controls = new ArrayList<>();
+        controls.addAll(List.of(notControl, orControl, andControl, xorControl, nandControl, norControl));
+
+        sourceInput = new Input(new boolean[]{true, false, true, true}, source);
+        destinationInput = new Input(new boolean[]{false, false, true, false}, destination);
+
+        notControlInput = new Input(new boolean[]{false}, notControl);
+        orControlInput = new Input(new boolean[]{false}, orControl);
+        andControlInput = new Input(new boolean[]{false}, andControl);
+        xorControlInput = new Input(new boolean[]{false}, xorControl);
+        nandControlInput = new Input(new boolean[]{false}, nandControl);
+        norControlInput = new Input(new boolean[]{false}, norControl);
+
+        outputObject = new Output(output);
+
+        LogicUnit logicUnit = new LogicUnit(source, destination, output, controls);
+    }
+
+    @Test
+    void testNot() {
+        notControlInput.setData(new boolean[]{true});
+
+        List<Node> queue = new ArrayList<>();
+        queue.addAll(List.of(sourceInput, destinationInput, notControlInput, orControlInput,
+                andControlInput, xorControlInput, nandControlInput, norControlInput));
+        run(queue);
+
+        assertArrayEquals(new boolean[]{false, true, false, false}, outputObject.getData());
+    }
+
+    @Test
+    void testOr() {
+        orControlInput.setData(new boolean[]{true});
+
+        List<Node> queue = new ArrayList<>();
+        queue.addAll(List.of(sourceInput, destinationInput, notControlInput, orControlInput,
+                andControlInput, xorControlInput, nandControlInput, norControlInput));
+        run(queue);
+
+        assertArrayEquals(new boolean[]{true, false, true, true}, outputObject.getData());
+    }
+
+    @Test
+    void testAnd() {
+        andControlInput.setData(new boolean[]{true});
+
+        List<Node> queue = new ArrayList<>();
+        queue.addAll(List.of(sourceInput, destinationInput, notControlInput, orControlInput,
+                andControlInput, xorControlInput, nandControlInput, norControlInput));
+        run(queue);
+
+        assertArrayEquals(new boolean[]{false, false, true, false}, outputObject.getData());
+    }
+
+    @Test
+    void testXor() {
+        xorControlInput.setData(new boolean[]{true});
+
+        List<Node> queue = new ArrayList<>();
+        queue.addAll(List.of(sourceInput, destinationInput, notControlInput, orControlInput,
+                andControlInput, xorControlInput, nandControlInput, norControlInput));
+        run(queue);
+
+        assertArrayEquals(new boolean[]{true, false, false, true}, outputObject.getData());
+    }
+
+    @Test
+    void testNand() {
+        nandControlInput.setData(new boolean[]{true});
+
+        List<Node> queue = new ArrayList<>();
+        queue.addAll(List.of(sourceInput, destinationInput, notControlInput, orControlInput,
+                andControlInput, xorControlInput, nandControlInput, norControlInput));
+        run(queue);
+
+        assertArrayEquals(new boolean[]{true, true, false, true}, outputObject.getData());
+    }
+
+    @Test
+    void testNor() {
+        norControlInput.setData(new boolean[]{true});
+
+        List<Node> queue = new ArrayList<>();
+        queue.addAll(List.of(sourceInput, destinationInput, notControlInput, orControlInput,
+                andControlInput, xorControlInput, nandControlInput, norControlInput));
+        run(queue);
+
+        assertArrayEquals(new boolean[]{false, true, false, false}, outputObject.getData());
+    }
+
+    @Test
+    void testMultiple1() {
+        testNor();
+        norControlInput.setData(new boolean[]{false});
+        testAnd();
+        andControlInput.setData(new boolean[]{false});
+        testOr();
+    }
+
+    @Test
+    void testMultiple2() {
+        testNot();
+        notControlInput.setData(new boolean[]{false});
+        testOr();
+        orControlInput.setData(new boolean[]{false});
+        testAnd();
+        andControlInput.setData(new boolean[]{false});
+        testXor();
+        xorControlInput.setData(new boolean[]{false});
+        testNand();
+        nandControlInput.setData(new boolean[]{false});
+        testNor();
+    }
+}
