bb4d1fc837e3f43009a85236d93ea7381b177b48
Jan Warchocki <j.warchocki@student.tudelft.nl>
2021-04-12 00:06:44 +0200

    Added test to check if shift registers work

---
+++ test/ShiftRegisterTest.java
@@ -0,0 +1,76 @@
+import main.BitStream;
+import main.circuits.memory.DFlipFlop;
+import main.control.Input;
+import main.gates.unary.NOT;
+import main.utils.ProcessRunner;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ShiftRegisterTest {
+
+    @Test
+    void test1() {
+        BitStream in = new BitStream(1);
+        BitStream q1 = new BitStream(1);
+        BitStream q2 = new BitStream(1);
+        BitStream q3 = new BitStream(1);
+        BitStream q4 = new BitStream(1);
+
+        BitStream clk = new BitStream(1);
+        BitStream enable = new BitStream(1);
+        enable.setData(new boolean[]{true});
+
+        DFlipFlop flipFlop1 = new DFlipFlop(in, clk, enable, q1, new BitStream(1), true);
+        DFlipFlop flipFlop2 = new DFlipFlop(q1, clk, enable, q2, new BitStream(1), true);
+        DFlipFlop flipFlop3 = new DFlipFlop(q2, clk, enable, q3, new BitStream(1), true);
+        DFlipFlop flipFlop4 = new DFlipFlop(q3, clk, enable, q4, new BitStream(1), true);
+
+        Input clock = new Input(new boolean[]{false}, clk);
+
+        clock.setData(new boolean[]{false});
+        ProcessRunner.run(clock);
+
+        Input input = new Input(new boolean[]{false}, in);
+        input.setData(new boolean[]{true});
+        clock.setData(new boolean[]{true});
+        ProcessRunner.run(input);
+        ProcessRunner.run(clock);
+
+        assertArrayEquals(new boolean[]{true}, q1.getData());
+        assertArrayEquals(new boolean[]{false}, q2.getData());
+        assertArrayEquals(new boolean[]{false}, q3.getData());
+        assertArrayEquals(new boolean[]{false}, q4.getData());
+
+        clock.setData(new boolean[]{false});
+        ProcessRunner.run(clock);
+        clock.setData(new boolean[]{true});
+        ProcessRunner.run(clock);
+
+        assertArrayEquals(new boolean[]{true}, q1.getData());
+        assertArrayEquals(new boolean[]{true}, q2.getData());
+        assertArrayEquals(new boolean[]{false}, q3.getData());
+        assertArrayEquals(new boolean[]{false}, q4.getData());
+
+        clock.setData(new boolean[]{false});
+        input.setData(new boolean[]{false});
+        ProcessRunner.run(clock, input);
+        clock.setData(new boolean[]{true});
+        ProcessRunner.run(clock);
+
+        assertArrayEquals(new boolean[]{false}, q1.getData());
+        assertArrayEquals(new boolean[]{true}, q2.getData());
+        assertArrayEquals(new boolean[]{true}, q3.getData());
+        assertArrayEquals(new boolean[]{false}, q4.getData());
+
+        clock.setData(new boolean[]{false});
+        ProcessRunner.run(clock);
+        clock.setData(new boolean[]{true});
+        ProcessRunner.run(clock);
+
+        assertArrayEquals(new boolean[]{false}, q1.getData());
+        assertArrayEquals(new boolean[]{false}, q2.getData());
+        assertArrayEquals(new boolean[]{true}, q3.getData());
+        assertArrayEquals(new boolean[]{true}, q4.getData());
+    }
+}
