204a9e33d5714ae2f60f10c9ff682f4224013e3d
Jan Warchocki <j.warchocki@student.tudelft.nl>
2021-04-15 21:23:53 +0200

    Declared the TFlipFlop class

--- src/main/circuits/memory/TFlipFlop.java
+++ src/main/circuits/memory/TFlipFlop.java
@@ -1,10 +1,57 @@
-//package main.circuits.memory;
-//
-//import main.BitStream;
-//import main.circuits.Circuit;
-//
-//public class TFlipFlop implements Circuit {
-//
-//    private BitStream T,
-//
-//}
+package main.circuits.memory;
+
+import main.BitStream;
+import main.circuits.Circuit;
+import main.gates.binary.AND;
+import main.gates.binary.OR;
+import main.gates.unary.NOT;
+
+public class TFlipFlop extends FlipFlop {
+
+    public TFlipFlop(BitStream T, BitStream clock, BitStream enable, BitStream preset, BitStream clear,
+                     BitStream Q, BitStream notQ, boolean risingEdge,
+                     String name, boolean inDebuggerMode, int debugDepth) {
+        super(T, clock, enable, preset, clear, Q, notQ, risingEdge, name, inDebuggerMode, debugDepth);
+
+        this.build();
+    }
+
+    public TFlipFlop(BitStream T, BitStream clock, BitStream enable, BitStream preset, BitStream clear,
+                     BitStream Q, BitStream notQ, boolean risingEdge,
+                     String name) {
+        this(T, clock, enable, preset, clear, Q, notQ, risingEdge, name, false, 0);
+    }
+
+    public TFlipFlop(BitStream T, BitStream clock, BitStream enable, BitStream preset, BitStream clear,
+                     BitStream Q, BitStream notQ, boolean risingEdge,
+                     boolean inDebuggerMode, int debugDepth) {
+        this(T, clock, enable, preset, clear, Q, notQ, risingEdge, "TFlipFlop", inDebuggerMode, debugDepth);
+    }
+
+    public TFlipFlop(BitStream T, BitStream clock, BitStream enable, BitStream preset, BitStream clear,
+                     BitStream Q, BitStream notQ, boolean risingEdge) {
+        this(T, clock, enable, preset, clear, Q, notQ, risingEdge, "TFlipFlop", false, 0);
+    }
+
+    @Override
+    public void build() {
+        boolean debugGates = this.getDebugDepth() > 0 ? this.isInDebuggerMode() : false;
+        int size = this.getInput().getSize();
+
+        BitStream tNotOut = new BitStream(size);
+        NOT tNot = new NOT(this.getInput(), tNotOut, "tNot", debugGates);
+
+        BitStream upperAndOut = new BitStream(size);
+        AND upperAnd = new AND(tNotOut, this.getQ(), upperAndOut, "upperAnd", debugGates);
+
+        BitStream lowerAndOut = new BitStream(size);
+        AND lowerAnd = new AND(this.getInput(), this.getNotQ(), lowerAndOut, "lowerAnd", debugGates);
+
+        BitStream orOut = new BitStream(size);
+        OR mainOr = new OR(upperAndOut, lowerAndOut, orOut, "mainOr", debugGates);
+
+        DFlipFlop dFlipFlop = new DFlipFlop(orOut, this.getClock(), this.getEnable(), this.getPreset(), this.getClear(),
+                this.getQ(), this.getNotQ(), this.isRisingEdge(), "dFlipFlop", debugGates, this.getDebugDepth() - 1);
+    }
+
+}
