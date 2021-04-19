5252763fb59191b62c5397db8f59a37d9071f2bf
Jan Warchocki <janwarchocki@gmail.com>
2021-04-19 20:46:00 +0200

    Further microprocessor development

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,27 +5,16 @@ import main.control.Output;
 import main.gates.binary.AND;
 import main.gates.binary.NOR;
 import main.gates.binary.OR;
+import main.memory.Storage;
 import main.utils.ProcessRunner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        BitStream streamIn1 = new BitStream(1);
-        BitStream streamIn2 = new BitStream(1);
-        BitStream out = new BitStream(1);
-
-        Input r = new Input(new boolean[]{false}, streamIn1, "R", false);
-        Input s = new Input(new boolean[]{true}, streamIn2, "S", false);
-
-        OR or = new OR(streamIn1, streamIn2, out);
-        AND and = new AND(streamIn1, streamIn2, out);
-
-        Output output = new Output(out, "Q", false);
-
-        ProcessRunner.run(r, s);
-
-        System.out.println(output.getData());
-
+        Storage microMapping = new Storage("./storage/main/microinstructions.stg", 256, 17);
+        microMapping.putData(0, new boolean[]{false, false, false, false, false, false, false});
+        microMapping.putData(1, new boolean[]{false, false, false, false, false, false, true});
+        microMapping.save();
     }
 }
--- src/main/circuits/Microprocessor.java
+++ src/main/circuits/Microprocessor.java
@@ -1,7 +1,9 @@
 package main.circuits;
 
 import main.BitStream;
+import main.circuits.memory.TFlipFlop;
 import main.control.Splitter;
+import main.memory.ROM;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,7 @@ public class Microprocessor implements Circuit {
 
     public static final int WORD_SIZE = 16;
     public static final int INSTRUCTION_SIZE = 8;
+    public static final int MAPPING_OUT_SIZE = 8;
 
     public Microprocessor(BitStream input, BitStream clock, BitStream reset, BitStream IR1In, BitStream IR2In,
                           BitStream microinstruction, BitStream intermediate, BitStream source, BitStream destination,
@@ -147,6 +150,44 @@ public class Microprocessor implements Circuit {
 
         Splitter instructionSplitter = new Splitter(instructionSplitterInputList, instructionSplitterOutList,
                 "instructionSplitter", debugGates);
+
+        BitStream mapRomOut = new BitStream(MAPPING_OUT_SIZE);
+        ROM mapRom = new ROM("./storage/main/microMapping.stg", instruction, mapRomOut, "mapRom", debugGates);
+
+        BitStream constant = new BitStream(1);
+        constant.setData(new boolean[]{true});
+        BitStream enable = new BitStream(1);
+        enable.setData(new boolean[]{true});
+
+        BitStream tFlipFlop0Q = new BitStream(1);
+        BitStream tFlipFlop0NotQ = new BitStream(1);
+        TFlipFlop tFlipFlop0 = new TFlipFlop(constant, this.clock, enable, new BitStream(1), this.reset,
+                tFlipFlop0Q, tFlipFlop0NotQ, true, "tFlipFlop0", debugGates, debugDepth - 1);
+
+        BitStream tFlipFlop1Q = new BitStream(1);
+        BitStream tFlipFlop1NotQ = new BitStream(1);
+        TFlipFlop tFlipFlop1 = new TFlipFlop(constant, tFlipFlop0NotQ, enable, new BitStream(1), this.reset,
+                tFlipFlop1Q, tFlipFlop1NotQ, true, "tFlipFlop1", debugGates, debugDepth - 1);
+
+        BitStream tFlipFlop2Q = new BitStream(1);
+        BitStream tFlipFlop2NotQ = new BitStream(1);
+        TFlipFlop tFlipFlop2 = new TFlipFlop(constant, tFlipFlop1NotQ, enable, new BitStream(1), this.reset,
+                tFlipFlop2Q, tFlipFlop2NotQ, true, "tFlipFlop2", debugGates, debugDepth - 1);
+
+        List<BitStream> counterSplitterInputList = new ArrayList<>();
+        counterSplitterInputList.addAll(List.of(tFlipFlop0Q, tFlipFlop1Q, tFlipFlop2Q,
+                new BitStream(MAPPING_OUT_SIZE - 3)));
+
+        BitStream counterSplitterOut = new BitStream(MAPPING_OUT_SIZE);
+        List<BitStream> counterSplitterOutList = new ArrayList<>();
+        counterSplitterOutList.add(counterSplitterOut);
+
+        Splitter counterSplitter = new Splitter(counterSplitterInputList, counterSplitterOutList,
+                "counterSplitter", debugGates);
+
+        BitStream addSubOutput = new BitStream(MAPPING_OUT_SIZE);
+        AddSubtract addSub = new AddSubtract(counterSplitterOut, mapRomOut, addSubOutput,
+                new BitStream(1), new BitStream(1), "addSub", debugGates, this.debugDepth - 1);
 
 
     }
